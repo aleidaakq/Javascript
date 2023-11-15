1020acd35a13b9aa759fbb8103dcd98dd4f7972e
aleidaakq <140558557+aleidaakq@users.noreply.github.com>
2023-11-15 18:33:16 +0000

    var compostas

---
+++ aula15/ambiente.js
@@ -0,0 +1,8 @@
+let num = [6, 8, 4, 1, 9]
+num[5]= 5
+num.push(7)
+num.sort()
+
+console.log (`Nosso vetor é o ${num} .`)
+console.log (`Nosso vetor tem ${num.length} espaços.`)
+console.log(num[0])
